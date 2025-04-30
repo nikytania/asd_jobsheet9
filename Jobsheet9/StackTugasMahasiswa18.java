79d7de778e7215a9c078ed1fd26d147ac0781bbf
nikytania <you@example.com>
2025-04-30 15:35:12 +0700

    stack

--- Jobsheet9/StackTugasMahasiswa18.java
+++ Jobsheet9/StackTugasMahasiswa18.java
@@ -67,4 +67,17 @@ public class StackTugasMahasiswa18 {
   public int jumlahTugas() {
     return top + 1;
   }
+  public String konversiDesimalKeBiner(int nilai) {
+    StackKonversi18 stack = new StackKonversi18();
+    while (nilai > 0) {
+      int sisa = nilai % 2;
+      stack.push(sisa);
+      nilai = nilai / 2;
+    }
+    String biner = new String();
+    while (!stack.isEmpty()) {
+      biner += stack.pop();
+    }
+    return biner;
+  }
 }
